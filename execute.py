72d5e8e571da4d723a0b59ad129812c889e9fb62
coding-jhuunn <codingatjhunn@gmail.com>
2025-05-17 23:04:05 +0800

    comment in execute.py test file

--- execute.py
+++ execute.py
@@ -1,7 +1,20 @@
-print("testing..")
+
+# write and read files
+# with open("testFolder/first_text.txt","w") as file:
+#     file.write("first text to write in a text")
 
 
-if 0 >2:
-    print("greater than 2")
-else:
-    print("note")
+# count the lines of a txt files
+# with open("my_file.txt", "r") as file:
+#     line_count = sum(1 for line in file)
+#     print("Number of lines:", line_count)
+
+
+# choose a specifc line in a txt files
+# line_number = 11
+# with open("redditFiles/testline.txt", "r") as file:
+#     lines = file.readlines()
+#     if line_number <= len(lines):
+#         print(f"Line {line_number}:", lines[line_number - 1].strip())
+#     else:
+#         print("That line doesn't exist.")

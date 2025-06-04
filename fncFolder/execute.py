7fed7d8d544513a9002428407ccfd07415e2d262
coding-jhuunn <codingatjhunn@gmail.com>
2025-06-04 11:43:28 +0800

    uncomment some lines

--- fncFolder/execute.py
+++ fncFolder/execute.py
@@ -1,20 +1,20 @@
 
-# write and read files
-# with open("testFolder/first_text.txt","w") as file:
-#     file.write("first text to write in a text")
+#write and read files
+with open("testFolder/first_text.txt","w") as file:
+    file.write("first text to write in a text")
 
 
-# count the lines of a txt files
-# with open("my_file.txt", "r") as file:
-#     line_count = sum(1 for line in file)
-#     print("Number of lines:", line_count)
+#count the lines of a txt files
+with open("my_file.txt", "r") as file:
+    line_count = sum(1 for line in file)
+    print("Number of lines:", line_count)
 
 
-# choose a specifc line in a txt files
-# line_number = 11
-# with open("redditFiles/testline.txt", "r") as file:
-#     lines = file.readlines()
-#     if line_number <= len(lines):
-#         print(f"Line {line_number}:", lines[line_number - 1].strip())
-#     else:
-#         print("That line doesn't exist.")
+#choose a specifc line in a txt files
+line_number = 11
+with open("redditFiles/testline.txt", "r") as file:
+    lines = file.readlines()
+    if line_number <= len(lines):
+        print(f"Line {line_number}:", lines[line_number - 1].strip())
+    else:
+        print("That line doesn't exist.")
